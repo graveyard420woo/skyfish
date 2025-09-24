fd617b05bf8a15188b86524bd17d52f4dc740620
graveyard <graveyard420woo@gmail.com>
2025-09-24 01:46:29 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    type: Phaser.CANVAS,
-    width: 800,
-    height: 600,
-    canvas: document.getElementById('game-canvas'),
-    scene: [GameScene]
-};
-
-const game = new Phaser.Game(config);
